30e9b3985894467af2297f562a315771752ea799
乔荦雯 <475204945@qq.com>
2019-01-06 14:59:52 +0800

    main文件直接可以读文件输出tree

--- src/Compiler/src/Main.java
+++ src/Compiler/src/Main.java
@@ -1,28 +1,50 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Scanner sc = new Scanner(System.in);
-		String words = sc.nextLine();
-		
-		ANTLRInputStream input = new ANTLRInputStream(words);
-		
-		MinijavaLexer lexer = new MinijavaLexer(input);
-		
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		
-		MinijavaParser parse = new MinijavaParser(tokens);
-		
-		ParseTree tree = parse.prog();
-		System.out.println(tree.toStringTree(parse));
-
-
+		FileInputStream fis = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null; 
+		try {
+			String str = "";
+			String str1 = "";
+			fis = new FileInputStream("binarysearch.txt");// FileInputStream
+			
+			isr = new InputStreamReader(fis);// InputStreamReader
+			br = new BufferedReader(isr);// 
+			while ((str = br.readLine()) != null) {
+				str1 += str + "\n";
+				
+			}
+			
+			
+			//System.out.println(str1);// 
+			ANTLRInputStream input = new ANTLRInputStream(str1);
+			MinijavaLexer lexer = new MinijavaLexer(input);		
+			CommonTokenStream tokens = new CommonTokenStream(lexer);	
+			MinijavaParser parse = new MinijavaParser(tokens);
+			ParseTree tree = parse.prog();
+			System.out.println(tree.toStringTree(parse));
+			
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("cannot find the file");
+		} catch (IOException e) {
+			System.out.println("failed to read the file");
+		} finally {
+			try {
+				br.close();
+				isr.close();
+				fis.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
+	
 }
